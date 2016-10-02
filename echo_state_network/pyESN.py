92b996bfd5ebb981e00ee9ef5cef37a9085f8a42
Ryosuke Sawada <Ryosuke Sawada>
2016-10-02 16:11:51 +0900

    arrange dataI/O format

--- echo_state_network/pyESN.py
+++ echo_state_network/pyESN.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pdb
 
 
 def correct_dimensions(s,targetlength):
--- echo_state_network/run_pyESN.py
+++ echo_state_network/run_pyESN.py
@@ -20,11 +20,11 @@ def run():
     parser.add_argument('-p','--parameter')
     parser.add_argument('-d','--drawflag',default=False,action='store_true')
     args = parser.parse_args()
-    inputfile = args.inputfile
-    outputfile = args.outputfile
-    parameter = args.parameter
+    inputfile = datadir + args.inputfile
+    outputfile = resultdir + args.outputfile
+    parameter = datadir + args.parameter
     drawflag = args.drawflag
-    mse,s_mse = run_pyESN(inputfile,outputfile,parameter,drawflag)
+    run_pyESN(inputfile,outputfile,parameter,drawflag)
 
 
 def load_parameter(filename):
@@ -40,42 +40,51 @@ def run_pyESN(inputfile,outputfile,parameter,drawflag=False):
     params = load_parameter(parameter)
     N = inputdata.shape[0]
     keysnum = inputdata.shape[1]
-    traintest_cutoff = np.ceil(0.7*N)
-    n_input = params['n_input']
-    n_output = params['n_output']
+    traintest_cutoff = int(np.ceil(0.7*N))
+    n_input = params['n_inputs']
+    n_output = params['n_outputs']
     if n_input+n_output != keysnum:
         print('Invalid input dimention')
         sys.exit()
     train_ctrl,test_ctrl = [],[]
-    train_output,train_output = [],[]
+    train_output,test_output = [],[]
     for key in inputdata.keys()[:n_input]:
-        train_ctrl.append(inputdata[key][:traintest_cutoff])
-        test_ctrl.append(inputdata[key][traintest_cutoff:])
+        if not train_ctrl:
+            train_ctrl = np.array(inputdata[key][:traintest_cutoff])
+            test_ctrl = np.array(inputdata[key][traintest_cutoff:])
+        else:
+            train_ctrl = np.vstack([train_ctrl,np.array(inputdata[key][:traintest_cutoff])])
+            test_ctrl = np.vstack([test_ctrl, np.array(inputdata[key][traintest_cutoff:])])
     for key in inputdata.keys()[n_input:]:
-        train_output.append(inputdata[key][:traintest_cutoff])
-        train_output.append(inputdata[key][traintest_cutoff:])
+        if not train_output:
+            train_output = np.array(inputdata[key][:traintest_cutoff])
+            test_output = np.array(inputdata[key][traintest_cutoff:])
+        else:
+            train_output = np.vstack([train_output,np.array(inputdata[key][:traintest_cutoff])])
+            test_output = np.vstack([test_output, np.array(inputdata[key][traintest_cutoff:])])
 
     #train_ctrl,train_output = control[:traintest_cutoff],output[:traintest_cutoff]
     #test_ctrl, test_output  = control[traintest_cutoff:],output[traintest_cutoff:]
 
-    esn = ESN(**load_parameter(parameter))
-    pred_train = esn.fit(train_ctrl,train_output,inspect=False)
+    esn = ESN(**params)
+    pred_train = esn.fit(train_ctrl,train_output)
     pred_test = esn.predict(test_ctrl)
+
+    #pd.DataFrame(pred_test).to_csv(outputfile)
 
     if drawflag:
         window_tr = range(len(train_output)/4,len(train_output)/4+2000)
         plt.subplot(211)
-        plt.plot(train_ctrl[window_tr,1],label='control')
+        plt.plot(train_ctrl[window_tr],label='control')
         plt.plot(train_output[window_tr],label='target')
         plt.plot(pred_train[window_tr],label='model')
         plt.legend(fontsize='x-small')
         plt.title('training (excerpt)')
         plt.ylim([-0.1,1.1])
         
-        #window_test = range(2000)
-        window_test = range(200)
+        window_test = range(400)
         plt.subplot(212)
-        plt.plot(test_ctrl[window_test,1],label='control')
+        plt.plot(test_ctrl[window_test],label='control')
         plt.plot(test_output[window_test],label='target')
         plt.plot(pred_test[window_test],label='model')
         plt.legend(fontsize='x-small')
